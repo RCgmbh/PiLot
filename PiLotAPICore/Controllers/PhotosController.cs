47cb42319cb94f39e3f8083be54e4ec2a5db47cd
M. Röthenmund <m.roethenmund@hotmail.com>
2022-02-14 19:54:05 +0100

    Put action added

--- PiLotAPICore/Controllers/PhotosController.cs
+++ PiLotAPICore/Controllers/PhotosController.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 
-using PiLot.API.ActionFilters;
 using PiLot.Data.Files;
 using PiLot.Model.Photos;
 using PiLot.Utils.Logger;
+
+using PiLot.API.ActionFilters;
+using PiLot.API.Workers;
 
 namespace PiLot.API.Controllers {
 
 	[Route(Program.APIROOT + "[controller]")]
 	[ApiController]
 	public class PhotosController : ControllerBase {
+
+		private const String QSDATEFORMAT = "yyyy-MM-dd";
 
 		[ServiceFilter(typeof(ReadAuthorizationFilter))]
 		[HttpGet]
@@ -22,6 +27,39 @@ namespace PiLot.API.Controllers {
 				Logger.Log(ex, "PhotosController.GetDailyPhotoGallery");
 				throw;
 			}
+		}
+
+		/// <summary>
+		/// Puts a photo to the server. The content must be nothing but the
+		/// pure bytes. No multipart data.
+		/// </summary>
+		/// <param name="day">The day as string in format yyyy-mm-dd</param>
+		/// <param name="fileName">the original Name of the image</param>
+		/// <returns></returns>
+		[ServiceFilter(typeof(WriteAuthorizationFilter))]
+		[HttpPut]
+		public ActionResult Put(String day, String fileName) {
+			Stream requestStream = this.Request.Body;
+			if (requestStream != null) {
+				try {
+					Date? date = null;
+					if (!String.IsNullOrEmpty(day)) {
+						date = new Date(DateTime.ParseExact(day, QSDATEFORMAT, null));
+					}
+					Byte[] bytesInStream = new Byte[requestStream.Length];
+					requestStream.Read(bytesInStream, 0, (int)bytesInStream.Length);
+					Logger.Log($"PhotoController recieved {bytesInStream.Length} bytes for {fileName}", LogLevels.DEBUG);
+					PhotosWorker.Instance.ProcessPhoto(new ImageData() {
+						Bytes = bytesInStream,
+						Day = date,
+						Name = fileName
+					});
+				} catch (Exception ex) {
+					Logger.Log(ex, "PhotosController.Put");
+					throw;
+				}
+			}
+			return this.Ok();
 		}
 	}
 }
