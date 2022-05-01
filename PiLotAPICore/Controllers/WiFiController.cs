dc424df74228746f968c0414b0f8010787f4b7d7
RCgmbh <martin@roethenmund.biz>
2022-05-01 21:47:16 +0200

    Comments

--- PiLotAPICore/Controllers/WiFiController.cs
+++ PiLotAPICore/Controllers/WiFiController.cs
@@ -16,27 +16,37 @@ namespace PiLot.API.Controllers {
 	public class WiFiController : ControllerBase {
 
 		/// <summary>
-		/// 
+		/// Gets a list of all known and of all currently available networks.
 		/// </summary>
 		[Route(Program.APIROOT + "[controller]")]
 		[HttpGet]
-		//[ServiceFilter(typeof(ReadAuthorizationFilter))]
+		[ServiceFilter(typeof(SystemAuthorizationFilter))]
 		public async Task<List<WiFiInfo>> GetNetworks() {
 			return await new WiFiHelper().GetNetworksAsync();
 		}
 
+		/// <summary>
+		/// Connects to the network with number. The number is taken
+		/// from the result of GetNetworks().
+		/// </summary>
 		[Route(Program.APIROOT + "[controller]/{number}/select")]
 		[HttpPut]
 		public String SelectNetwork(Int32 number){
 			return new WiFiHelper().SelectNetwork(number);
 		}
 
+		/// <summary>
+		/// Adds a new network defined by ssid and passphrase.
+		/// </summary>
 		[Route(Program.APIROOT + "[controller]")]
 		[HttpPost]
 		public String AddNetwork(String ssid, string passphrase){
 			return new WiFiHelper().AddNetwork(ssid, passphrase);
 		}
 
+		/// <summary>
+		/// Deletes a specific network.
+		/// </summary>
 		[Route(Program.APIROOT + "[controller]/{number}")]
 		[HttpDelete]
 		public String DeleteNetwork(Int32 number){
