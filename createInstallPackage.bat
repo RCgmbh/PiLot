88ae859e8da31b1f0ff38597faf976504a39bf69
M. Röthenmund <m.roethenmund@hotmail.com>
2022-07-31 18:57:30 +0200

    separate config from data

--- createInstallPackage.bat
+++ createInstallPackage.bat
@@ -14,14 +14,14 @@ rd /s /q pilotinstall\resources\pilotapi\App_Data\publishing
 rd /s /q pilotinstall\resources\pilotapi\App_Data\routes
 copy pilotinstall\resources\pilotapi\App_Data\global\app.default.json pilotinstall\resources\pilotapi\App_Data\global\app.json
 del  pilotinstall\resources\pilotapi\App_Data\global\app.*.json
-copy pilotinstall\resources\pilotapi\App_Data\sensors\sensors.default.json pilotinstall\resources\pilotapi\App_Data\sensors\sensors.json
-del  pilotinstall\resources\pilotapi\App_Data\sensors\sensors.*.json
-copy pilotinstall\resources\pilotapi\App_Data\tiles\tileSources.default.json pilotinstall\resources\pilotapi\App_Data\tiles\tileSources.json
-del  pilotinstall\resources\pilotapi\App_Data\tiles\tileSources.*.json
-copy pilotinstall\resources\pilotapi\App_Data\authorization.default.json pilotinstall\resources\pilotapi\App_Data\authorization.json
-del  pilotinstall\resources\pilotapi\App_Data\authorization.*.json
-copy pilotinstall\resources\pilotapi\App_Data\users.default.json pilotinstall\resources\pilotapi\App_Data\users.json
-del  pilotinstall\resources\pilotapi\App_Data\users.*.json
+copy pilotinstall\resources\pilotapi\config\sensors.default.json pilotinstall\resources\pilotapi\config\sensors.json
+del  pilotinstall\resources\pilotapi\config\sensors.*.json
+copy pilotinstall\resources\pilotapi\config\tileSources.default.json pilotinstall\resources\pilotapi\config\tileSources.json
+del  pilotinstall\resources\pilotapi\config\tileSources.*.json
+copy pilotinstall\resources\pilotapi\config\authorization.default.json pilotinstall\resources\pilotapi\config\authorization.json
+del  pilotinstall\resources\pilotapi\config\authorization.*.json
+copy pilotinstall\resources\pilotapi\config\users.default.json pilotinstall\resources\pilotapi\config\users.json
+del  pilotinstall\resources\pilotapi\config\users.*.json
 
 
 mkdir pilotinstall\resources\pilotphotoswatcher
@@ -35,7 +35,7 @@ dotnet build PiLotSensors -o pilotinstall\resources\pilotsensors -c release -r l
 copy /y pilotinstall\resources\pilotsensors\app.default.config pilotinstall\resources\pilotsensors\PiLot.Sensors.dll.config
 del /s /q pilotinstall\resources\pilotsensors\app.*.config
 del /s /q pilotinstall\resources\pilotsensors\app.config
-move /y pilotinstall\resources\pilotsensors\sensors.example.json pilotinstall\resources\pilotapi\App_Data\sensors\
+move /y pilotinstall\resources\pilotsensors\sensors.example.json pilotinstall\resources\pilotapi\config\
 
 mkdir pilotinstall\resources\pilotweb
 xcopy /s /r /i  PiLotWeb\* pilotinstall\resources\pilotweb\
