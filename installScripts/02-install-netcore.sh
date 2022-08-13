9c3900af74ceff1058ac2a64553242942798784e
M. Röthenmund <m.roethenmund@hotmail.com>
2022-08-13 09:57:27 +0200

    Move to .net standard 2.1

--- installScripts/02-install-netcore.sh
+++ installScripts/02-install-netcore.sh
@@ -3,7 +3,8 @@ if [ `whoami` != root ]; then
     echo Please run this script using sudo
     exit
 fi
-wget https://download.visualstudio.microsoft.com/download/pr/93874c40-bd2d-4a7d-bbb5-716b161594c0/f698334222759b065f5da4e8915ae982/aspnetcore-runtime-6.0.2-linux-arm.tar.gz
+#wget https://download.visualstudio.microsoft.com/download/pr/93874c40-bd2d-4a7d-bbb5-716b161594c0/f698334222759b065f5da4e8915ae982/aspnetcore-runtime-6.0.2-linux-arm.tar.gz
+wget  https://download.visualstudio.microsoft.com/download/pr/cf567026-a29a-41aa-bc3a-e4e1ad0df480/0925d411e8e09e31ba7a39a3eb0e29af/aspnetcore-runtime-6.0.8-linux-arm.tar.gz
 mkdir -p /opt/dotnet
 tar -zxf aspnetcore-runtime-6.0.2-linux-arm.tar.gz -C /opt/dotnet
 ln -s /opt/dotnet/dotnet /usr/bin
