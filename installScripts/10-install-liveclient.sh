2229a06188c46e82489d533705757336971aca8c
M. Röthenmund <m.roethenmund@hotmail.com>
2022-08-21 18:33:10 +0200

    minor changes for install script

--- installScripts/10-install-liveclient.sh
+++ installScripts/10-install-liveclient.sh
@@ -11,8 +11,11 @@ mkdir /opt/pilotliveclient/		# application binaries
 # install application
 cp -r resources/pilotliveclient/* /opt/pilotliveclient/
 
+# create config link in /etc/pilot
+ln /opt/pilotliveclient/config.json /etc/pilot/liveClient.json
+
 # configure service, don't enable or start
 cp resources/liveClient.service /etc/systemd/system/liveClient.service
 systemctl daemon-reload
 
-echo Done. Please update /etc/pilot/liveClient.config
+echo Done. Please update /etc/pilot/liveClient.json
