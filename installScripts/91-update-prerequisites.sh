7c51e3e6f353eadd2afbe4a0ebdec03b1387d82f
M. Röthenmund <m.roethenmund@hotmail.com>
2022-09-24 13:11:33 +0200

    Add update scripts

---
+++ installScripts/91-update-prerequisites.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This installs git and the dotnet sdk, creates a folder for git repos and clones the pilot repo.
+# These actions are required to later fetch the latest versions of the PiLot applications, and build
+# and deploy them locally.
+# The script assumes to have a user pi with a /home/pi home directory.
+
+set -e
+
+if [ `whoami` != root ]; then
+    echo Please run this script using sudo
+    exit
+fi
+
+mkdir /home/pi/pilot/repos
+cd /home/pi/pilot/repos
+apt update
+apt -y upgrade
+apt -y install git
+git clone https://github.com/RCgmbh/PiLot
+cd /home/pi
+wget https://download.visualstudio.microsoft.com/download/pr/451f282f-dd26-4acd-9395-36cc3a9758e4/f5399d2ebced2ad9640db6283aa9d714/dotnet-sdk-6.0.401-linux-arm.tar.gz
+tar -zxf dotnet-sdk-6.0.401-linux-arm.tar.gz -C /opt/dotnet
---
+++ installScripts/92-update-webapp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This will get the latest sources and update all PiLot components. Git must be installed and the PiLot repository must have been created before.
+# The dotnet SDK must have been installed before. 
+#
+# See 91-update-prerequisites.sh
+
+set -e
+
+if [ `whoami` != root ]; then
+    echo Please run this script using sudo
+    exit
+fi
+
+cd /home/pi/repos/PiLot
+git pull
+echo "Copy website data"
+sudo rsync -vr PiLotWeb/* /var/www/html/pilot/ --exclude 'js/Config*.js'
+echo "Copy website data done."
+echo "Build and install latest API Version"
+systemctl stop gpsLogger
+systemctl stop pilotApi
+cp -R /opt/pilotapi/config /opt/pilotapi/config_bak
+cp /opt/pilotapi/PiLot.API.dll.config /opt/pilotapi/PiLot.API.dll.config.bak
+dotnet build PiLotAPICore -o /opt/pilotapi -c release -r linux-arm --no-self-contained
+mv /opt/pilotapi/config_bak /opt/pilotapi/config
+mv /opt/pilotapi/PiLot.API.dll.config.bak /opt/pilotapi/PiLot.API.dll.config
+rm -r /opt/pilotapi/App_Data
+echo "API installed"
+systemctl start pilotApi
+systemctl start gpsLogger
+
+echo "Done"
