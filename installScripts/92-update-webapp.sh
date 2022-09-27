0615194ce318778a4eefe0fb2ce745901031672d
M. Röthenmund <m.roethenmund@hotmail.com>
2022-09-27 19:53:04 +0200

    Preserve links when restoring config

--- installScripts/92-update-webapp.sh
+++ installScripts/92-update-webapp.sh
@@ -20,9 +20,11 @@ echo "Copy website data done."
 echo "Build and install latest API Version"
 systemctl stop gpsLogger
 systemctl stop pilotApi
-cp -R /opt/pilotapi/config /opt/pilotapi/config_bak
-cp /opt/pilotapi/PiLot.API.dll.config /opt/pilotapi/PiLot.API.dll.config.bak
+mv /opt/pilotapi/config /opt/pilotapi/config_bak
+mv /opt/pilotapi/PiLot.API.dll.config /opt/pilotapi/PiLot.API.dll.config.bak
 dotnet build PiLotAPICore -o /opt/pilotapi -c release -r linux-arm --no-self-contained
+rm -r /opt/pilotapi/config
+rm /opt/pilotapi/PiLot.API.dll.config
 mv /opt/pilotapi/config_bak /opt/pilotapi/config
 mv /opt/pilotapi/PiLot.API.dll.config.bak /opt/pilotapi/PiLot.API.dll.config
 rm -r /opt/pilotapi/App_Data
