e467fb881859b97a72d294101fd7a319ddaa300c
M. Röthenmund <m.roethenmund@hotmail.com>
2022-10-01 22:25:49 +0200

    New proxies

---
+++ PiLotAPIProxy/RoutesProxy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using PiLot.Model.Nav;
+
+namespace PiLot.APIProxy {
+
+	/// <summary>
+	/// Proxy class to access the /Routes endpoint
+	/// </summary>
+	public class RoutesProxy {
+
+		private const String CONTROLLERURL = "/Routes";
+
+		private PiLotHttpClient httpClient;
+		private String apiControllerUrl;
+
+		/// <summary>
+		/// Creates a new RoutesProxy instance, which can be used to 
+		/// read data from and send data to the Routes API
+		/// </summary>
+		/// <param name="pApiUrl">The API Root url, such as http://localhost/pilotapi/api/v1, without ending /</param>
+		/// <param name="pLoginHelper">Pass the one and only LoginHelper within the application, or null</param>
+		public RoutesProxy(String pApiUrl, LoginHelper pLoginHelper) {
+			this.apiControllerUrl = pApiUrl + CONTROLLERURL;
+			this.httpClient = new PiLotHttpClient(pLoginHelper);
+		}
+
+		/// <summary>
+		/// This takes a Route and saves it to the api.
+		/// </summary>
+		/// <param name="pRoute">Route to save</param>
+		/// <returns>true, if the operation succeeded</returns>
+		public async Task<Boolean> PutRouteAsync(Route pRoute) {
+			String jsonString = JsonSerializer.Serialize(pRoute);
+			return await this.httpClient.PutAsync(jsonString, this.apiControllerUrl);
+		}
+	}
+}
---
+++ PiLotAPIProxy/SettingsProxy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PiLot.APIProxy {
+
+	/// <summary>
+	/// Proxy class to access the /Settings endpoint
+	/// </summary>
+	public class SettingsProxy {
+
+		private const String CONTROLLERURL = "/Settings";
+
+		private PiLotHttpClient httpClient;
+		private String apiControllerUrl;
+
+		/// <summary>
+		/// Creates a new SettingsProxy instance, which can be used to 
+		/// read data from and send data to the Settings API
+		/// </summary>
+		/// <param name="pApiUrl">The API Root url, such as http://localhost/pilotapi/api/v1, without ending /</param>
+		/// <param name="pLoginHelper">Pass the one and only LoginHelper within the application, or null</param>
+		public SettingsProxy(String pApiUrl, LoginHelper pLoginHelper) {
+			this.apiControllerUrl = pApiUrl + CONTROLLERURL;
+			this.httpClient = new PiLotHttpClient(pLoginHelper);
+		}
+
+		/// <summary>
+		/// Selects the current route
+		/// </summary>
+		/// <param name="pRouteId">The id of the current route</param>
+		/// <returns>true, if the operation succeeded</returns>
+		public async Task<Boolean> SelectRoute(Int32? pRouteId) {
+			String url = $"{this.apiControllerUrl}/activeRouteId?routeId={pRouteId}";
+			return await this.httpClient.PutAsync(String.Empty, url);
+		}
+
+		/// <summary>
+		/// Selects the current boat config
+		/// </summary>
+		/// <param name="pName">The name of the current boat config</param>
+		/// <returns>true, if the operation succeeded</returns>
+		public async Task<Boolean> SelectBoatConfig(String pName) {
+			String url = $"{this.apiControllerUrl}/currentBoatConfigName?name={pName}";
+			return await this.httpClient.PutAsync(String.Empty, url);
+		}
+	}
+}
