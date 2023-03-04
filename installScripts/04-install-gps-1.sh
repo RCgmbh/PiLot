f9fc6c2ae16f24feb52293380fbd7c33e9e6636e
M. Röthenmund <m.roethenmund@hotmail.com>
2023-03-04 22:37:48 +0100

    install gpsd-tools instead of python-gps

--- installScripts/04-install-gps-1.sh
+++ installScripts/04-install-gps-1.sh
@@ -11,7 +11,7 @@ echo "dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elev
 
 systemctl stop serial-getty@ttyS0.service
 systemctl disable serial-getty@ttyS0.service
-apt install -y gpsd gpsd-clients python-gps
+apt install -y gpsd gpsd-clients gpsd-tools
 
 echo "enable_uart=1" >> /boot/config.txt
 echo "Done with part 1. Please reboot, then start 05-install-gps-2.sh"
