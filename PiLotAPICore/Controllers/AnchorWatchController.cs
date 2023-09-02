54b46697dbf69207d0705787bc0e9796d824f4f0
M. Röthenmund <m.roethenmund@hotmail.com>
2023-09-02 17:50:20 +0200

    clean up usings

--- PiLotAPICore/Controllers/AnchorWatchController.cs
+++ PiLotAPICore/Controllers/AnchorWatchController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 using PiLot.API.ActionFilters;
 using PiLot.API.Workers;
-using PiLot.Data.Files;
 using PiLot.Model.Nav;
 
 namespace PiLot.API.Controllers {
