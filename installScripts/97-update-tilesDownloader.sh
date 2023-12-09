8f39306326608a34cfeae4444289e7e49274f846
M. Röthenmund <m.roethenmund@hotmail.com>
2023-12-09 19:50:32 +0100

    Add installer for Tiles downloader, downgrade to .net6

---
+++ installScripts/97-update-tilesDownloader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This will get the latest sources and update the PiLot Tiles Downloader. Git must be installed and the PiLot repository must have been created before.
+# The dotnet SDK must have been installed before. 
+#
+# See 91-update-prerequisites.sh
+
+if [ `whoami` != root ]; then
+    echo Please run this script using sudo
+    exit
+fi
+
+cd /home/pi/repos/PiLot
+git pull
+echo "Build and install latest PiLotTilesDownloader version"
+dotnet build PiLotTilesDownloader -o /opt/pilottilesdownloader -c release -r linux-arm --no-self-contained
+echo "New PiLotTilesDownloader installed"
+echo "Done"
