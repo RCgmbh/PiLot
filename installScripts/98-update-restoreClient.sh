ad1003f47419d1d33917285201e0a9d9aed71b6d
RCgmbh <martin@roethenmund.biz>
2025-03-11 18:28:06 +0100

    Add installer for restore client

---
+++ installScripts/98-update-restoreClient.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This installs or updates the restore client which allows to restore data from a backup set.
+# This can be used for the first installation as well as for an update.
+
+if [ `whoami` != root ]; then
+    echo Please run this script using sudo
+    exit
+fi
+
+# Prepare directories
+mkdir -p /opt/pilotrestoreclient	#PiLot restore client binaries
+
+# pull and build application
+cd /home/pi/repos/PiLot
+git pull
+echo "Build and install latest restore client version"
+dotnet build PiLotRestoreClient -o /opt/pilotrestoreclient -c release -r linux-arm --no-self-contained
+echo "New restore client built"
+echo "Done"
