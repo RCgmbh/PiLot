1803b67c9216087eeebcc13d4eda7e897790c13f
RCgmbh <martin@roethenmund.biz>
2025-12-18 19:47:28 +0100

    move to .net 10

--- installScripts/02-install-platform.sh
+++ installScripts/02-install-platform.sh
@@ -7,6 +7,8 @@ fi
 mkdir -p /opt/dotnet
 # wget  https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/6.0.36/aspnetcore-runtime-6.0.36-linux-arm64.tar.gz
 # tar -zxf aspnetcore-runtime-6.0.36-linux-arm64.tar.gz -C /opt/dotnet
+# wget https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/10.0.1/aspnetcore-runtime-10.0.1-linux-arm.tar.gz
+# tar -zxf aspnetcore-runtime-10.0.1-linux-arm.tar.gz
 wget https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.11/aspnetcore-runtime-9.0.11-linux-arm64.tar.gz
 tar -zxf aspnetcore-runtime-9.0.11-linux-arm64.tar.gz -C /opt/dotnet
 
