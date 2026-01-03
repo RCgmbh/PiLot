4e7617dddb80e69f7d246707e1d7cccfb98aec76
RCgmbh <martin@roethenmund.biz>
2026-01-03 11:08:58 +0100

    Work on release script

--- installScripts/updatePrebuilt/buildRelease.ps1
+++ installScripts/updatePrebuilt/buildRelease.ps1
@@ -13,23 +13,29 @@ for ( $i = 0; $i -lt $targets.Count; $i++)
     $version = $targets[$i][0]
     $architecture = $targets[$i][1]
     $targetPath = "{0}\{1}_{2}" -f $target, $version, $architecture
+    $unwantedItems = @(
+        "$targetPath\App_Data",
+        "$targetPath\config",
+        "$targetPath\*.pdb",
+        "$targetPath\*.config",
+        "$targetPath\appsettings.*",
+        "$targetPath\config.*"
+    )
     if (Test-Path -path $targetPath) {
         Remove-Item -Path $targetPath -Force -Recurse
     }    
     Write-Host ("Starting build for version: $version, architecture: $architecture") -ForegroundColor Blue
     dotnet publish $csprojPath -o $targetPath -r $architecture -f $version --no-self-contained
     if (Test-Path -path $targetPath) {
-        Remove-Item -Path "$targetPath\App_Data" -Force -Recurse
-        Remove-Item -Path "$targetPath\config" -Force -Recurse
-        del "$targetPath\*.pdb"
-        del "$targetPath\*.config"
-        del "$targetPath\appsetting.*"
-        Start-Sleep -Seconds 5
+        Remove-Item -Path $unwantedItems -Force -Recurse -ErrorAction SilentlyContinue
         $tarFilePath = "{0}\{1}_{2}_{3}.tar" -f $target, $prefix, $version, $architecture
-        7z a -r -ttar $tarFilePath $targetPath
-        $targzFilePath = "$tarFilePath.gz" 
+        $targzFilePath = "$tarFilePath.gz"
+        Remove-Item -Path $tarFilePath, $targzFilePath -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 5
+        7z a -r -ttar $tarFilePath $targetPath\*
         7z a -r -tgzip -mx9 $targzFilePath $tarFilePath
         $releaseFiles += $targzFilePath
+        Remove-Item -Path $tarFilePath
         Write-Host "Release built at $targetPath" -ForegroundColor Green
     }else{
         Write-Host ("Build failed for version: $version, architecture: $architecture") -ForegroundColor red
