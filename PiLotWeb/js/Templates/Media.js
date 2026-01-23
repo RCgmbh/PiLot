e2c9ec67d0f73e1b4449294a303fb8bfe3da399b
RCgmbh <martin@roethenmund.biz>
2026-01-23 11:34:35 +0100

    removing css float

--- PiLotWeb/js/Templates/Media.js
+++ PiLotWeb/js/Templates/Media.js
@@ -6,42 +6,44 @@ PiLot.Templates.Media = {
 	gamesOverviewPage: `
 		<div class="contentPadding">
 			<h1>Games</h1>
-			<a href="games/snake/index.html" class="tile big" hidden>
-				<div>
-					<img src="img/iconSnake.svg" />
-					<span class="label">Snake</span>
-				</div>		
-			</a>
-			<a href="games/2048/index.html" class="tile big">
-				<div>
-					<img src="img/icon2048.svg" />
-					<span class="label">2048</span>
-				</div>		
-			</a>
-			<a href="games/1024/index.html" class="tile big">
-				<div>
-					<img src="img/icon1024.svg" />
-					<span class="label">1024</span>
-				</div>		
-			</a>
-			<a href="games/re-wire/index.html" class="tile big">
-				<div>
-					<img src="img/iconRewire.svg" />
-					<span class="label">Re-wire</span>
-				</div>		
-			</a>
-			<a href="games/choch/index.html" class="tile big">
-				<div>
-					<img src="img/iconChoch.svg" />
-					<span class="label">ЧОЧ</span>
-				</div>		
-			</a>
-			<a href="games/gameOfLife/index.html" class="tile big">
-				<div>
-					<img src="img/iconGameOfLife.svg" />
-					<span class="label">Game of Life</span>
-				</div>
-			</a>
+			<div class="flexRowWrap">
+				<a href="games/snake/index.html" class="tile big" hidden>
+					<div>
+						<img src="img/iconSnake.svg" />
+						<span class="label">Snake</span>
+					</div>		
+				</a>
+				<a href="games/2048/index.html" class="tile big">
+					<div>
+						<img src="img/icon2048.svg" />
+						<span class="label">2048</span>
+					</div>		
+				</a>
+				<a href="games/1024/index.html" class="tile big">
+					<div>
+						<img src="img/icon1024.svg" />
+						<span class="label">1024</span>
+					</div>		
+				</a>
+				<a href="games/re-wire/index.html" class="tile big">
+					<div>
+						<img src="img/iconRewire.svg" />
+						<span class="label">Re-wire</span>
+					</div>		
+				</a>
+				<a href="games/choch/index.html" class="tile big">
+					<div>
+						<img src="img/iconChoch.svg" />
+						<span class="label">ЧОЧ</span>
+					</div>		
+				</a>
+				<a href="games/gameOfLife/index.html" class="tile big">
+					<div>
+						<img src="img/iconGameOfLife.svg" />
+						<span class="label">Game of Life</span>
+					</div>
+				</a>
+			</div>
 		</div>
 	`,
 
